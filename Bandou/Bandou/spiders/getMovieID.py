0f3926b6d36860249e79c629073e7410b56f1863
Derek.S <derekseli@outlook.com>
2020-07-16 17:43:34 +0800

    change urls

--- Bandou/Bandou/spiders/getMovieID.py
+++ Bandou/Bandou/spiders/getMovieID.py
@@ -7,22 +7,26 @@ from items import SubjectID
 class GetMovieIDSpider(scrapy.Spider):
 
     # 默认url
-    url_str = "https://movie.a.com/j/new_search_subjects?sort=U&range=0,10&tags=电影&start="
+    url_str = "https://movie.a.com/j/new_search_subjects?sort=R&range=0,10&tags=电影&start=0"
 
     name = 'getMovieID'
     allowed_domains = ['a.com']
-    start_urls = [url_str + "0"]
+    start_urls = ["https://movie.a.com/j/new_search_subjects?sort=R&range=0,10&tags=电影&start=" + str(x) for x in range(2060, 10000, 20)]
+
+    custom_settings = {
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 1
+    }
+
 
     def parse(self, response):
         rep = json.loads(response.body)
-
         subject = SubjectID()
         for each in rep["data"]:
             subject["subject_id"] = each["id"]
             subject["title"] = each["title"]
             yield subject
 
-        for x in range(20, 10000, 20):
-            bid = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(11))
-            url = self.url_str + str(x)
-            yield scrapy.Request(url, cookies={"bid": bid})
+        # for x in range(20, 10000, 20):
+        #     bid = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(11))
+        #     url = self.url_str + str(x)
+        #     yield scrapy.Request(url, cookies={"bid": bid}, dont_filter=True)
