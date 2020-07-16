49b6b59bc0296716c5c8763974f9ae858cfd3e77
Derek.S <derekseli@outlook.com>
2020-07-16 15:52:55 +0800

    get movie id

--- Bandou/Bandou/spiders/getMovieID.py
+++ Bandou/Bandou/spiders/getMovieID.py
@@ -2,8 +2,9 @@ import scrapy
 import json
 import random
 import string
+from items import SubjectID
 
-class GetmovieidSpider(scrapy.Spider):
+class GetMovieIDSpider(scrapy.Spider):
 
     # 默认url
     url_str = "https://movie.a.com/j/new_search_subjects?sort=U&range=0,10&tags=电影&start="
@@ -13,9 +14,15 @@ class GetmovieidSpider(scrapy.Spider):
     start_urls = [url_str + "0"]
 
     def parse(self, response):
-        print(json.loads(response.body))
+        rep = json.loads(response.body)
 
-        for x in range(20, 60, 20):
+        subject = SubjectID()
+        for each in rep["data"]:
+            subject["subject_id"] = each["id"]
+            subject["title"] = each["title"]
+            yield subject
+
+        for x in range(20, 10000, 20):
             bid = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(11))
             url = self.url_str + str(x)
             yield scrapy.Request(url, cookies={"bid": bid})
