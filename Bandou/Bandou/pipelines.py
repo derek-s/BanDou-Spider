8288363d3e289d183297a1663238fcd353a5a160
Derek.S <derekseli@outlook.com>
2020-07-16 15:51:56 +0800

    write mongodb

--- Bandou/Bandou/pipelines.py
+++ Bandou/Bandou/pipelines.py
@@ -7,7 +7,23 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+from pymongo import MongoClient
+from settings import DB_HOSTS, DB_PORT, DB_PWD, DB_USERNAME
 
 class BandouPipeline:
+
+    def open_spider(self, spider):
+        # mongodb
+        if spider.name == "getMovieID":
+            client = MongoClient(DB_HOSTS, DB_PORT)
+            db = client.Bandou
+            db.authenticate(DB_USERNAME, DB_PWD)
+            self.collection = db.SubjectID
+
     def process_item(self, item, spider):
+        if spider.name == "getMovieID":
+            try:
+                self.collection.insert(dict(item))
+            except:
+                pass
         return item
