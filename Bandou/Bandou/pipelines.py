fa9089771793dfd0fdfeaacda276de4177b52087
Derek.S <derekseli@outlook.com>
2020-07-17 10:54:19 +0800

    add AnimeID/Meta db ORM

--- Bandou/Bandou/pipelines.py
+++ Bandou/Bandou/pipelines.py
@@ -22,6 +22,10 @@ class BandouPipeline:
             self.collection = db.TVPlayID
         if spider.name == "getTVPlayInfo":
             self.collection = db.TVPlayInfo
+        if spider.name == "getAnimeID":
+            self.collection = db.AnimeID
+        if spider.name == "getAnimeInfo":
+            self.collection = db.AnimeInfo
 
     def process_item(self, item, spider):
         if spider.name == "getMovieID":
@@ -40,6 +44,16 @@ class BandouPipeline:
             except Exception as e:
                 print(e)
         if spider.name == "getTVPlayInfo":
+            try:
+                self.collection.insert(dict(item))
+            except Exception as e:
+                print(e)
+        if spider.name == "getAnimeID":
+            try:
+                self.collection.insert(dict(item))
+            except Exception as e:
+                print(e)
+        if spider.name == "getAnimeInfo":
             try:
                 self.collection.insert(dict(item))
             except Exception as e:
